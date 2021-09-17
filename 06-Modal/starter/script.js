212ba03a347aa8c4278ddfe034edbf42ada744c1
swsas1786 <swsas1786@gmail.com>
2021-09-17 14:30:00 -0400

    test

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const btnShowModal = document.querySelectorAll('.show-modal');
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const closeModal = document.querySelector('.close-modal');
+
+
+for (let i = 0; i < btnShowModal.length; i++) {
+    btnShowModal[i].addEventListener('click', function () {
+        modal.classList.remove('hidden');
+        overlay.classList.remove('hidden');
+    })
+
+}
+
